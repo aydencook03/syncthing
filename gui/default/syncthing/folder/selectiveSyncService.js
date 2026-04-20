32f130e61c9d12303579aad9f315b73b8fd8f2a2
Ayden Cook <cookayden2003@gmail.com>
2026-04-20 04:48:28 +0000

    Refactor: strip bloat from selectiveSyncService.js

--- gui/default/syncthing/folder/selectiveSyncService.js
+++ gui/default/syncthing/folder/selectiveSyncService.js
@@ -5,12 +5,10 @@
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
 angular.module('syncthing.core')
-    .factory('selectiveSyncService', ['$http', '$q', function ($http, $q) {
+    .factory('selectiveSyncService', ['$http', function ($http) {
         'use strict';
 
         var INCLUDE_LINE = '#include .stselective';
-
-        // _state[folderId] = { enabled: bool, paths: Set, touched: bool }
         var _state = {};
 
         function st(folderId) {
@@ -20,53 +18,40 @@ angular.module('syncthing.core')
             return _state[folderId];
         }
 
-        // Load current state from the backend (.stselective + .stignore include check).
         function load(folderId) {
             return $http.get(urlbase + '/db/selective?folder=' + encodeURIComponent(folderId))
                 .then(function (r) {
                     var s = st(folderId);
-                    // Only import from disk if the user hasn't made changes yet.
                     if (!s.touched) {
                         s.enabled = !!r.data.enabled;
                         s.paths = new Set(r.data.paths || []);
                     }
-                    return s;
                 });
         }
 
-        // Save: write .stselective and manage the #include line in .stignore.
         function save(folderId) {
             var s = st(folderId);
             var paths = s.enabled ? Array.from(s.paths).sort() : [];
 
-            // 1. Write (or clear) .stselective
-            return $http.post(
-                urlbase + '/db/selective?folder=' + encodeURIComponent(folderId),
-                { paths: paths }
-            ).then(function () {
-                // 2. Read current .stignore
-                return $http.get(urlbase + '/db/ignores?folder=' + encodeURIComponent(folderId));
-            }).then(function (r) {
-                var lines = (r.data && r.data.ignore) || [];
-                var hasInclude = lines.indexOf(INCLUDE_LINE) !== -1;
-
-                if (s.enabled && !hasInclude) {
-                    lines = [INCLUDE_LINE].concat(lines);
-                } else if (!s.enabled && hasInclude) {
-                    lines = lines.filter(function (l) { return l !== INCLUDE_LINE; });
-                } else {
-                    return; // no change needed
-                }
-
-                return $http.post(
-                    urlbase + '/db/ignores?folder=' + encodeURIComponent(folderId),
-                    { ignore: lines }
-                );
-            });
+            return $http.post(urlbase + '/db/selective?folder=' + encodeURIComponent(folderId), { paths: paths })
+                .then(function () {
+                    return $http.get(urlbase + '/db/ignores?folder=' + encodeURIComponent(folderId));
+                }).then(function (r) {
+                    var lines = (r.data && r.data.ignore) || [];
+                    var has = lines.indexOf(INCLUDE_LINE) !== -1;
+                    if (s.enabled && !has) {
+                        lines = [INCLUDE_LINE].concat(lines);
+                    } else if (!s.enabled && has) {
+                        lines = lines.filter(function (l) { return l !== INCLUDE_LINE; });
+                    } else {
+                        return;
+                    }
+                    return $http.post(urlbase + '/db/ignores?folder=' + encodeURIComponent(folderId), { ignore: lines });
+                });
         }
 
-        function enable(folderId)  { var s = st(folderId); s.enabled = true;  s.touched = true; }
-        function disable(folderId) { var s = st(folderId); s.enabled = false; s.touched = true; }
+        function enable(folderId)    { var s = st(folderId); s.enabled = true;  s.touched = true; }
+        function disable(folderId)   { var s = st(folderId); s.enabled = false; s.touched = true; }
         function isEnabled(folderId) { return !!st(folderId).enabled; }
         function isTouched(folderId) { return !!(_state[folderId] && _state[folderId].touched); }
 
@@ -79,6 +64,7 @@ angular.module('syncthing.core')
         function deselectPath(folderId, path) {
             var s = st(folderId);
             s.paths.delete(path);
+            // Also remove any children
             var prefix = path + '/';
             s.paths.forEach(function (p) { if (p.indexOf(prefix) === 0) s.paths.delete(p); });
             s.touched = true;
@@ -90,6 +76,7 @@ angular.module('syncthing.core')
             var s = _state[folderId];
             if (!s) return false;
             if (s.paths.has(path)) return true;
+            // Check if any ancestor is selected
             var parts = path.split('/');
             for (var i = parts.length - 1; i > 0; i--) {
                 var anc = parts.slice(0, i).join('/');
@@ -113,18 +100,8 @@ angular.module('syncthing.core')
             s.touched = true;
         }
 
-        return {
-            load: load,
-            save: save,
-            enable: enable,
-            disable: disable,
-            isEnabled: isEnabled,
-            isTouched: isTouched,
-            selectPath: selectPath,
-            deselectPath: deselectPath,
-            getSelectedPaths: getSelectedPaths,
-            isPathSelected: isPathSelected,
-            isPathPartial: isPathPartial,
-            clearSelections: clearSelections
-        };
+        return { load: load, save: save, enable: enable, disable: disable, isEnabled: isEnabled,
+                 isTouched: isTouched, selectPath: selectPath, deselectPath: deselectPath,
+                 getSelectedPaths: getSelectedPaths, isPathSelected: isPathSelected,
+                 isPathPartial: isPathPartial, clearSelections: clearSelections };
     }]);
